0b79a8448a14cf824a9ef3639c82cc360a866913
Christian Decker <decker.christian@gmail.com>
2014-10-15 15:54:47 +0200

    Ported inv_tracker example to new networking stack.

--- examples/inv_tracker.py
+++ examples/inv_tracker.py
@@ -4,28 +4,18 @@ Created on Mar 4, 2013
 @author: cdecker
 '''
 
-from bitcoin import PooledBitcoinProtocolFactory
-from functools import partial
-from twisted.internet import reactor
+from bitcoin import network
 
-class InvTracker(PooledBitcoinProtocolFactory):
-
-    def buildProtocol(self, addr):
-        connection = PooledBitcoinProtocolFactory.buildProtocol(self, addr)
-        connection.handlers['inv'].append(partial(self.on_inv_received, connection))
-        return connection
-        
-    def on_inv_received(self, connection, inv):
-        for item in inv.hashes:
-            print connection.key, item[0], item[1].encode("hex")
+def on_inv_received(connection, inv):
+    for item in inv.hashes:
+        print connection.host, item[0], item[1].encode("hex")
 
 def start():
-    factory = InvTracker(1000)
-    desiredPort = 8333
-    reactor.listenTCP(desiredPort, factory) #@UndefinedVariable
-
-    print "Listening to port %d" % factory.port
-    reactor.run() #@UndefinedVariable
+    client = network.GeventNetworkClient()
+    client.register_handler('inv', on_inv_received)
+    network.ClientBehavior(client)
+    client.connect(('seed.bitcoinstats.com', 8333))
+    client.run_forever()
     
 if __name__=="__main__":
     start()
