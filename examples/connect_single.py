e6cb1e596d01883acd861042a616f1ace0a4a91a
Christian Decker <cdecker@tik.ee.ethz.ch>
2015-03-12 13:21:50 +0100

    setup.py and examples are now pep8 compliant as well.

--- examples/connect_single.py
+++ examples/connect_single.py
@@ -1,9 +1,10 @@
-__author__ = 'cdecker'
-
-
 from bitcoin import network
 from socket import gethostbyname
 import logging
+
+
+__author__ = 'cdecker'
+
 
 network_client = network.GeventNetworkClient()
 ip = gethostbyname('seed.bitcoinstats.com')
@@ -13,7 +14,9 @@ network_client.connect((ip, 8333))
 def send_version(connection, message):
     v = network.messages.VersionPacket()
     v.addr_from = network.messages.Address('127.0.0.1', True, 8333, 1)
-    v.addr_recv = network.messages.Address(connection.host[0], True, connection.host[1], 1)
+    v.addr_recv = network.messages.Address(
+        connection.host[0], True, connection.host[1], 1
+    )
     v.best_height = 0
     connection.send('version', v)
 
@@ -24,11 +27,17 @@ def send_verack(connection, message):
 
 def handle_message(connection, message):
     logging.info('%s message from %s:%d', message.type, *connection.host)
-    print '%s message from %s:%d' % (message.type, connection.host[0], connection.host[1])
+    print '%s message from %s:%d' % (
+        message.type, connection.host[0], connection.host[1]
+    )
 
 
-network_client.register_handler(network.ConnectionEstablishedEvent.type, send_version)
-network_client.register_handler(network.messages.VersionPacket.type, send_verack)
+network_client.register_handler(
+    network.ConnectionEstablishedEvent.type, send_version
+)
+network_client.register_handler(
+    network.messages.VersionPacket.type, send_verack
+)
 
 for t in network.messages.parsers:
     network_client.register_handler(t, handle_message)
--- examples/inv_tracker.py
+++ examples/inv_tracker.py
@@ -6,9 +6,11 @@ Created on Mar 4, 2013
 
 from bitcoin import network
 
+
 def on_inv_received(connection, inv):
     for item in inv.hashes:
         print connection.host, item[0], item[1].encode("hex")
+
 
 def start():
     client = network.GeventNetworkClient()
@@ -16,6 +18,7 @@ def start():
     network.ClientBehavior(client)
     client.connect(('seed.bitcoinstats.com', 8333))
     client.run_forever()
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     start()
